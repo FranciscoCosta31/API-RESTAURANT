7c4e68c3cdf02807b356508b6ad30f4a3906b27e
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-08 13:00:43 -0300

    Criando a estrutura do projeto

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { ProductsController } from "../controllers/products-controller" // Importe a CLASSE
+
+const productsRoutes = Router()
+
+const productsController = new ProductsController()
+
+productsRoutes.get("/", productsController.index)
+
+export { productsRoutes }
