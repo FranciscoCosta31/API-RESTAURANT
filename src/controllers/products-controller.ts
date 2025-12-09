ac25448b706208bd0a9feb2a38c965ba69170a67
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-09 11:09:33 -0300

    Validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,23 @@
 import { NextFunction, Request, Response } from "express"
+import { z } from "zod"
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "OK" })
+    } catch (error) {
+      next(error)
+    }
+  }
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodyschema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, "Price must be greater than 0"),
+      })
+      const { name, price } = bodyschema.parse(request.body)
+
+      return response.status(201).json({ name, price })
     } catch (error) {
       next(error)
     }
