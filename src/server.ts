741c4fce118d4e8e641e2797584d47ca5034b7d6
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-09 10:04:36 -0300

    Instalando e Configurando Knex.js

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express"
-
 import { router } from "./routes"
+import { errorHandling } from "./middlewares/error-handling"
 
 const PORT = 3333
 const app = express()
 app.use(express.json())
 app.use(router)
 
+app.use(errorHandling)
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
