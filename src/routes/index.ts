240bda53320109be59c3cd6dc226cb46347ea2a4
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-09 15:30:38 -0300

    Listando mesas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,11 @@ import { Router } from "express"
 
 import { productsRoutes } from "./products-routes"
 
+import { tablesRoutes } from "./tables-routes"
+
 const router = Router()
 
 router.use("/products", productsRoutes)
+router.use("/tables", tablesRoutes)
 
 export { router }
