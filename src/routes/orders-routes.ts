da4bbefcd1cf0eab59a5f576fa31944d34a9992f
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-15 17:40:12 -0300

    Rota Para Listar Pedidos Por Mesa

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,5 +6,6 @@ const orderRouter = Router()
 const ordersController = new OrdersController()
 
 orderRouter.post("/", ordersController.create)
+orderRouter.get("/table-session/:id", ordersController.index)
 
 export { orderRouter }
