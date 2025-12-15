3d52df206c68b5f3201f328d80ec4cdfef7867f2
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-15 11:36:37 -0300

    Abrindo a Mesa.

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,8 +1,21 @@
 import { NextFunction, Request, Response } from "express"
+import { z } from "zod"
+import { knex } from "../database/knex"
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+
+      const { table_id } = bodySchema.parse(request.body)
+
+      await knex<TablesSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      })
+
       return response.status(201).json()
     } catch (error) {
       next(error)
@@ -11,4 +24,3 @@ class TablesSessionsController {
 }
 
 export { TablesSessionsController }
-// Logic to create a table session
