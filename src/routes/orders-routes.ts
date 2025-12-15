ef4f9a711f0c81d00a057044165baf8cc77d945a
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-15 17:58:26 -0300

    Listando pedidos por mesa

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,6 +6,6 @@ const orderRouter = Router()
 const ordersController = new OrdersController()
 
 orderRouter.post("/", ordersController.create)
-orderRouter.get("/table-session/:id", ordersController.index)
+orderRouter.get("/table-session/:table_session_id", ordersController.index)
 
 export { orderRouter }
