9ed75a4ea23b39f28eba7c696d99d7ddedd803b2
“wolfcf6” <francisco.uberbh@outlook.com>
2025-12-15 16:45:47 -0300

    Tratando Dados do Pedido.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express"
 import { tablesSessionsRoutes } from "./tables-sessions-routes"
 import { productsRoutes } from "./products-routes"
 import { tablesRoutes } from "./tables-routes"
+import { orderRouter } from "./orders-routes"
 
 const router = Router()
 
 router.use("/tables-sessions", tablesSessionsRoutes)
 router.use("/products", productsRoutes)
 router.use("/tables", tablesRoutes)
+router.use("/orders", orderRouter)
 
 export { router }
---
+++ src/routes/orders-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+
+import { OrdersController } from "@/controllers/orders-controller"
+
+const orderRouter = Router()
+const ordersController = new OrdersController()
+
+orderRouter.post("/", ordersController.create)
+
+export { orderRouter }
